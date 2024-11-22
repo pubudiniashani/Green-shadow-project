cf94d617f2d134828dbb9172c73148ca121c48d4
pubudiniashani <pubudiniashani2000@gmail.com>
2024-11-22 23:58:44 +0530

    vehicle service layer

--- backend/green-shadow-backend/src/main/java/lk/ijse/greenshadowbackend/service/VehicleService.java
+++ backend/green-shadow-backend/src/main/java/lk/ijse/greenshadowbackend/service/VehicleService.java
@@ -1,4 +1,13 @@
 package lk.ijse.greenshadowbackend.service;
 
+
+import lk.ijse.greenshadowbackend.dto.impl.VehicleDTO;
+
+import java.util.List;
+
 public interface VehicleService {
+    void saveVehicle(VehicleDTO vehicleDTO);
+    void updateVehicle(String vehicleId, VehicleDTO vehicleDTO);
+    void deleteVehicle(String vehicleId);
+    List<VehicleDTO> getAllVehicles();
 }
--- backend/green-shadow-backend/src/main/java/lk/ijse/greenshadowbackend/service/impl/VehicleServiceImpl.java
+++ backend/green-shadow-backend/src/main/java/lk/ijse/greenshadowbackend/service/impl/VehicleServiceImpl.java
@@ -1,6 +1,32 @@
 package lk.ijse.greenshadowbackend.service.impl;
 
+import lk.ijse.greenshadowbackend.dto.impl.VehicleDTO;
 import lk.ijse.greenshadowbackend.service.VehicleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Service
+@Transactional
 public class VehicleServiceImpl implements VehicleService {
+    @Override
+    public void saveVehicle(VehicleDTO vehicleDTO) {
+
+    }
+
+    @Override
+    public void updateVehicle(String vehicleId, VehicleDTO vehicleDTO) {
+
+    }
+
+    @Override
+    public void deleteVehicle(String vehicleId) {
+
+    }
+
+    @Override
+    public List<VehicleDTO> getAllVehicles() {
+        return null;
+    }
 }
